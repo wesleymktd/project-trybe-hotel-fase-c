b3d58125740e75315a01734be60b989e2117c24e
wesleymktd <wesleymktd@gmail.com>
2023-08-21 10:58:23 -0300

    adicionado State na model City req01

--- src/TrybeHotel/Models/City.cs
+++ src/TrybeHotel/Models/City.cs
@@ -9,6 +9,7 @@ namespace TrybeHotel.Models
         [Key]
         public int CityId { get; set; }
         public string Name { get; set; } = "";
+        public string State { get; set; } = "";
         [InverseProperty("City")]
         public ICollection<Hotel>? Hotels { get; set; }
 
